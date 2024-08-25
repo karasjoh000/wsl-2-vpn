97ad0afa57b019475a6c3902ec566aa3d0e25617
John Karasev <john.karasev@intel.com>
2024-08-25 01:31:14 -0700

    add auto proxy config

--- update-on-vpn-connect.ps1
+++ update-on-vpn-connect.ps1
@@ -1,3 +1,12 @@
+$ProxyInfo = @"
+export http_proxy="http_proxy here or exclude line"
+export ftp_proxy="..."
+export https_proxy="..."
+export socks_proxy="..."
+export no_proxy="no proxy here or exlude line"
+"@
+
+
 ## Find all profiles that are wsl 2
 # get a list of profiles 
 $wslOutput = wsl.exe -l -v | Out-String
@@ -26,11 +35,50 @@ foreach ($entry in $dataRowsClean) {
 ## Add routes for wsl 2 profiles in order to fix vpn issue
 ## credits go to https://live.paloaltonetworks.com/t5/globalprotect-discussions/globalprotect-blocks-the-network-traffic-of-wsl2/m-p/507821/highlight/true#M2955
 foreach ($profile in $wsl2profiles) {
-    $var=C:\Windows\System32\wsl.exe -d $profile -e /bin/bash --noprofile --norc -c "/sbin/ip -o -4 addr list eth0"
+    $var= wsl.exe -d $profile -e /bin/bash --noprofile --norc -c "/sbin/ip -o -4 addr list eth0"
     $wsl_addr = $var.split()[6].split('/')[0]
-    $var2 = C:\Windows\System32\wsl.exe -d $profile -e /bin/bash --noprofile --norc -c "/sbin/ip -o route show table main default"
+    $var2 = wsl.exe -d $profile -e /bin/bash --noprofile --norc -c "/sbin/ip -o route show table main default"
     $wsl_gw = $var2.split()[2]
     $ifindex = Get-NetRoute -DestinationPrefix $wsl_gw/32 | Select-Object -ExpandProperty "IfIndex"
     $routemetric = Get-NetRoute -DestinationPrefix $wsl_gw/32 | Select-Object -ExpandProperty "RouteMetric"
     route add $wsl_addr mask 255.255.255.255 $wsl_addr metric $routemetric if $ifindex
+}
+
+## Get dns server from the vpn adapter or default whichever is default (Lowest InterfaceMetric)
+# Assuming the default interface has a dnsserver
+# Assuming /etc/wsl.conf: [network]\ngenerateResolvConf = false
+$firstIf = Get-NetIPInterface | Where-Object { $_.ConnectionState -eq "Connected" } | Sort-Object -Property InterfaceMetric | Select-Object -First 1
+$firstIfIndex = $firstIf.InterfaceIndex
+$dnsServers = Get-DnsClientServerAddress -InterfaceIndex $firstIfIndex -AddressFamily IPv4 | Select-Object -ExpandProperty ServerAddresses
+$dnsServersStringArray = $dnsServers | ForEach-Object { "nameserver $_" }
+foreach ($profile in $wsl2profiles) {
+    wsl.exe -d $profile -u root -e /bin/bash --noprofile --norc -c "rm -rf /etc/resolv.conf"
+    foreach ($dns in $dnsServersStringArray) {
+        wsl.exe -d $profile -u root -e /bin/bash --noprofile --norc -c "echo $dns >> /etc/resolv.conf"
+    }
+}
+
+## set proxy varaiables in /etc/environment and /etc/profile
+# only matches palto alto and cisco anyconnect vpns. Feel free to add any other into match.
+$firstIfDescription = Get-NetAdapter | Where-Object { $_.ifIndex -eq $firstIfIndex } | Where-Object { ($_.InterfaceDescription -match "PANGP Virtual Ethernet|Cisco AnyConnect") -and ($_.Status -eq "Up") }
+foreach ($profile in $wsl2profiles) {
+    if ($firstIfDescription -eq $null -or $firstIfDescription.Count -eq 0) {
+        # remove proxy info 
+        wsl.exe -d $profile -u root -e /bin/bash --noprofile --norc -c "sed -i '/export http_proxy/d' /etc/environment"
+        wsl.exe -d $profile -u root -e /bin/bash --noprofile --norc -c "sed -i '/export ftp_proxy/d' /etc/environment"
+        wsl.exe -d $profile -u root -e /bin/bash --noprofile --norc -c "sed -i '/export https_proxy/d' /etc/environment"
+        wsl.exe -d $profile -u root -e /bin/bash --noprofile --norc -c "sed -i '/export socks_proxy/d' /etc/environment"
+        wsl.exe -d $profile -u root -e /bin/bash --noprofile --norc -c "sed -i '/export no_proxy/d' /etc/environment"
+        wsl.exe -d $profile -u root -e /bin/bash --noprofile --norc -c "sed -i '/export http_proxy/d' /etc/profile"
+        wsl.exe -d $profile -u root -e /bin/bash --noprofile --norc -c "sed -i '/export ftp_proxy/d' /etc/profile"
+        wsl.exe -d $profile -u root -e /bin/bash --noprofile --norc -c "sed -i '/export https_proxy/d' /etc/profile"
+        wsl.exe -d $profile -u root -e /bin/bash --noprofile --norc -c "sed -i '/export socks_proxy/d' /etc/profile"
+        wsl.exe -d $profile -u root -e /bin/bash --noprofile --norc -c "sed -i '/export no_proxy/d' /etc/profile"
+    
+    } else {
+        foreach ($proxy in $ProxyInfo -split "`r`n|`n|`r") {
+            wsl.exe -d $profile -u root -e /bin/bash --noprofile --norc -c "echo $proxy >> /etc/environment"
+            wsl.exe -d $profile -u root -e /bin/bash --noprofile --norc -c "echo $proxy >> /etc/profile"
+        }
+    }
 }
